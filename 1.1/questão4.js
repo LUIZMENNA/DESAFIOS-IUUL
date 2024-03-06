f21c94a75db8fc83cf0261285504f7617c95d052
Luiz.Menna <48499689+LUIZMENNA@users.noreply.github.com>
2024-03-06 19:30:57 -0300

    corrigi erros
    .

--- 1.1/questão4.js
+++ 1.1/questão4.js
@@ -1,75 +1,101 @@
-import { Vertice } from "./questao1.js";
+export class Turma {
+    #listaDeAlunos = [];
 
-export class Poligono {
-    #vertices;
-    constructor(vertices) {
-        if (!this.ePoligonoValido(vertices)) {
-            throw new Error('O polígono deve ter pelo menos 3 vértices.');
+    get listaDeAlunos (){
+        return this.#listaDeAlunos;
+    }
+
+    inserirAluno(...alunos) {
+        this.#listaDeAlunos.push(...alunos);
+    }
+
+    removerAluno(matricula) {
+        const alunoParaRemover = this.#listaDeAlunos.findIndex(objeto => objeto.matricula === matricula);
+
+        if (alunoParaRemover !== -1) {
+            const alunoRemovido = this.#listaDeAlunos[alunoParaRemover];
+            console.log(`O aluno ${alunoRemovido.nome} foi removido com sucesso.`);
+            this.#listaDeAlunos.splice(alunoParaRemover, 1);
+        } else {
+            console.log(`Aluno com matrícula ${matricula} não encontrado.`);
         }
+    }
 
-        this.#vertices = [];
-        for (let vertice of vertices) {
-            this.addVertice(vertice);
+    //o usuário define a matricula de quem ele quer mudar a nota, podendo alterar as duas notas ou só uma
+    lancarNota(matricula, notaP1, notaP2) {
+        const alunoParaAlterar = this.#listaDeAlunos.find(aluno => aluno.matricula === matricula);
+    
+        if (alunoParaAlterar) {
+            alunoParaAlterar.notaP1 = notaP1;
+            alunoParaAlterar.notaP2 = notaP2;
+        } else {
+            console.log(`Aluno com matrícula ${matricula} não encontrado.`);
         }
     }
+    
 
-    // Método para verificar se o polígono é válido (pelo menos 3 vértices)
-    ePoligonoValido(vertices) {
-        return vertices.length >= 3;
-    }
-
-    // Método para adicionar um novo vértice ao polígono
-    addVertice(vertice) {
-        for (let v of this.#vertices) {
-            if (v.x === vertice.x && v.y === vertice.y) {
-                return false; // O vértice já existe no polígono
-            }
+    imprimirListaDeAlunos() {
+        const listaOrdenada = this.#listaDeAlunos.sort((x, y) => {
+            let a = x.nome.toUpperCase(),
+                b = y.nome.toUpperCase();
+            return a === b ? 0 : a > b ? 1 : -1;
+        });
+    
+        console.log(`
+            —---------------------------------------
+            Matrícula  Nome         P1     P2     NF
+            —---------------------------------------
+        `);
+    
+        for (const aluno of listaOrdenada) {
+            console.log(`        ${aluno.matricula}         ${aluno.nome}        ${aluno.notaP1}      ${aluno.notaP2}      ${aluno.calcularNotaFinal()}`);
         }
-        this.#vertices.push(vertice);
-        return true;
+    
+        console.log(`
+            —----------------------------------
+        `);
     }
-
-    // Método para calcular o perímetro do polígono
-    perimetro() {
-        let perimetro = 0;
-        for (let i = 0; i < this.#vertices.length; i++) {
-            const v1 = this.#vertices[i];
-            const v2 = this.#vertices[(i + 1) % this.#vertices.length]; // O próximo vértice
-            perimetro += Math.sqrt(Math.pow(v2.x - v1.x, 2) + Math.pow(v2.y - v1.y, 2));
-        }
-        return perimetro;
-    }
-
-    // Método para retornar a quantidade de vértices do polígono
-    qtdVertices() {
-        return this.#vertices.length;
-    }
+    
 }
 
-// Função para ler valores do usuário e criar um vértice
-function lerVertice() {
-    const x = parseFloat(prompt("Digite a coordenada x do vértice:"));
-    const y = parseFloat(prompt("Digite a coordenada y do vértice:"));
-    return new Vertice(x, y);
-}
+export class Aluno {
+    static autoincrement = 0;//o primeiro aluno registrado começará com 1
+    #matricula; 
+    #nome;
+    #P1 = 0;
+    #P2 = 0;
 
-// Função para ler valores do usuário e criar um polígono
-function lerPoligono() {
-    const vertices = [];
-    while (true) {
-        const vertice = lerVertice();
-        vertices.push(vertice);
-        const adicionarOutro = prompt("Deseja adicionar outro vértice? (sim/não)").toLowerCase();
-        if (adicionarOutro !== 'sim') {
-            break;
-        }
+    constructor(nome) {
+        Aluno.autoincrement++;
+        this.#matricula = Aluno.autoincrement; 
+        this.#nome = nome;
     }
-    return new Poligono(vertices);
+
+    get matricula (){
+        return this.#matricula;
+    }
+
+    get nome () {
+        return this.#nome;
+    }
+
+    get nota() {
+        return `As notas foram 1°: ${this.#P1}; 2°: ${this.#P2};`;
+    }    
+
+    calcularNotaFinal() {
+        let NF = 0;
+    
+        if (this.notaP1 !== undefined && this.notaP2 !== undefined) {
+            NF = (this.notaP1 + this.notaP2) / 2;
+        } else if (this.notaP1 !== undefined) {
+            NF = this.notaP1 / 2;
+        } else if (this.notaP2 !== undefined) {
+            NF = this.notaP2 / 2;
+        } else {
+            NF = 0;
+        }
+    
+        return NF.toFixed(1);
+    }   
 }
-
-// Ler valores do usuário para criar um polígono
-const poligono = lerPoligono();
-
-// Chamar os métodos implementados na classe
-console.log(`Perímetro do polígono: ${poligono.perimetro()}`);
-console.log(`Quantidade de vértices do polígono: ${poligono.qtdVertices()}`);
--- 1.1/questão5.js
+++ 1.1/questão5.js
@@ -1,75 +1,155 @@
-import { Vertice } from "./questao1.js";
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
 
-export class Poligono {
-    #vertices;
-    constructor(vertices) {
-        if (!this.ePoligonoValido(vertices)) {
-            throw new Error('O polígono deve ter pelo menos 3 vértices.');
-        }
+export class Cliente {
+    #nome;
+    #cpf;
+    #dataDeNascimento;
+    #rendaMensal;
+    #estadoCivil;
+    #dependentes;
 
-        this.#vertices = [];
-        for (let vertice of vertices) {
-            this.addVertice(vertice);
-        }
+    constructor (nome, cpf, dataDeNascimento, rendaMensal, estadoCivil, dependentes) {
+        this.#nome = nome;
+        this.#cpf = cpf;
+        this.#dataDeNascimento = dataDeNascimento;
+        this.#rendaMensal = rendaMensal;
+        this.#estadoCivil = estadoCivil;
+        this.#dependentes = dependentes;
     }
 
-    // Método para verificar se o polígono é válido (pelo menos 3 vértices)
-    ePoligonoValido(vertices) {
-        return vertices.length >= 3;
+    toString() {
+        return `Cliente { 
+            Nome: ${this.#nome}, 
+            CPF: ${this.#cpf}, 
+            Data de Nascimento: ${this.#dataDeNascimento}, 
+            Renda Mensal: ${this.#rendaMensal}, 
+            Estado Civil: ${this.#estadoCivil}, 
+            Dependentes: ${this.#dependentes} 
+        }`;
     }
+}
 
-    // Método para adicionar um novo vértice ao polígono
-    addVertice(vertice) {
-        for (let v of this.#vertices) {
-            if (v.x === vertice.x && v.y === vertice.y) {
-                return false; // O vértice já existe no polígono
+export function obterNome() {
+    let nome;
+    let padrao = /^[a-zA-Z]{5,}$/;
+
+    do {
+        nome = prompt('Digite seu nome: ');
+
+        if (nome.match(padrao)) {
+            return nome;
+        } else {
+            console.log('Por favor, insira um nome no formato válido. Deve ter pelo menos 5 letras.');
+        }
+    } while (true);
+
+
+}
+
+
+export function obterCPF() {
+    let cpf;
+    let padraoRegex = /^\d{11}$/;
+
+    do {
+        cpf = prompt('Digite seu CPF no seguinte formato: XXXXXXXXXXX (apenas números): ');
+
+        if (cpf.match(padraoRegex)) {
+            return parseInt(cpf);
+        } else {
+            console.log('Por favor, insira um CPF no formato válido. ');
+        }
+    } while (true); 
+}
+
+export function obterDataDeNascimento() {
+    let data;
+    let padraoRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(19\d{2}|20\d{2})$/;
+
+    do {
+        data = prompt('Digite sua data de nascimento (DD/MM/AAAA): ');
+
+        // Adicionando a verificação de correspondência e ajuste do formato da data
+        if (data.match(padraoRegex)) {
+            let partesData = data.split('/');
+            let diaNascimento = parseInt(partesData[0], 10);
+            let mesNascimento = parseInt(partesData[1], 10) - 1;
+            let anoNascimento = parseInt(partesData[2], 10);
+
+            let dataNascimento = new Date(anoNascimento, mesNascimento, diaNascimento);
+            let hoje = new Date();
+            let idade = hoje.getFullYear() - dataNascimento.getFullYear();
+
+            if (
+                hoje.getMonth() < mesNascimento ||
+                (hoje.getMonth() === mesNascimento && hoje.getDate() < diaNascimento)
+            ) {
+                idade--;
             }
-        }
-        this.#vertices.push(vertice);
-        return true;
-    }
 
-    // Método para calcular o perímetro do polígono
-    perimetro() {
-        let perimetro = 0;
-        for (let i = 0; i < this.#vertices.length; i++) {
-            const v1 = this.#vertices[i];
-            const v2 = this.#vertices[(i + 1) % this.#vertices.length]; // O próximo vértice
-            perimetro += Math.sqrt(Math.pow(v2.x - v1.x, 2) + Math.pow(v2.y - v1.y, 2));
+            if (idade >= 18) {
+                return dataNascimento;
+            } else {
+                console.log('Você deve ter pelo menos 18 anos. ');
+            }
+        } else {
+            console.log('Por favor, insira uma data no formato válido. ');
         }
-        return perimetro;
-    }
-
-    // Método para retornar a quantidade de vértices do polígono
-    qtdVertices() {
-        return this.#vertices.length;
-    }
+    } while (true);
 }
 
-// Função para ler valores do usuário e criar um vértice
-function lerVertice() {
-    const x = parseFloat(prompt("Digite a coordenada x do vértice:"));
-    const y = parseFloat(prompt("Digite a coordenada y do vértice:"));
-    return new Vertice(x, y);
-}
+export function obterRendaMensal() {
+    let rendaMensal;
+    let padraoRegex = /^\d+(,\d{2})?$/;
 
-// Função para ler valores do usuário e criar um polígono
-function lerPoligono() {
-    const vertices = [];
-    while (true) {
-        const vertice = lerVertice();
-        vertices.push(vertice);
-        const adicionarOutro = prompt("Deseja adicionar outro vértice? (sim/não)").toLowerCase();
-        if (adicionarOutro !== 'sim') {
-            break;
+    do {
+        rendaMensal = prompt('Digite a sua Renda Mensal no seguinte formato: XXXXXXX,XX (apenas números e vírgula para separar os valores decimais): ');
+
+        if (padraoRegex.test(rendaMensal)) {
+            let rendaMensalFormatada = parseFloat(rendaMensal.replace(',', '.'));
+            
+            if (!isNaN(rendaMensalFormatada) && rendaMensalFormatada > 0) {
+                return rendaMensalFormatada;
+            } else {
+                console.log('Por favor, insira um valor válido. ');
+            }
+        } else {
+            console.log('Por favor, insira um valor no formato válido. ');
         }
-    }
-    return new Poligono(vertices);
+    } while (true);
 }
 
-// Ler valores do usuário para criar um polígono
-const poligono = lerPoligono();
+export function obterEstadoCivil () {
+    let estadoCivil;
+    let padraoRegex = /^[CSVĐcsvđ]$/;
 
-// Chamar os métodos implementados na classe
-console.log(`Perímetro do polígono: ${poligono.perimetro()}`);
-console.log(`Quantidade de vértices do polígono: ${poligono.qtdVertices()}`);
+    do {
+        estadoCivil = prompt('Digite o seu estado civil no seguinte formato: C, S, V ou D (maiúsculo ou minúsculo): ');
+
+        if (estadoCivil.match(padraoRegex)) {
+            return estadoCivil;
+        } else {
+            console.log('Por favor, insira um valor no formato válido. ');
+        }
+    } while (true); 
+}
+
+export function obterNumeroDeDependentes () {
+    let numeroDeDependentes;
+    let padraoRegex = /^\d+$/;
+
+    do {
+        numeroDeDependentes = prompt('Digite o seu número de dependentes: ');
+
+        if (numeroDeDependentes.match(padraoRegex)) {
+            return numeroDeDependentes;
+        } else {
+            console.log('Por favor, insira um valor no válido. ');
+        }
+    } while (true); 
+}
+
+const cliente = new Cliente(obterNome(), obterCPF(), obterDataDeNascimento(), obterRendaMensal(), obterEstadoCivil(), obterNumeroDeDependentes());
+
+console.log(cliente.toString());
